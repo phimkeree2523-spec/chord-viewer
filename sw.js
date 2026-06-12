95a3e1c3294ce64ff86486a9168b8097dd8d56a6
phimkeree2523-spec <phimkeree2523@gmail.com>
2026-06-12 15:19:46 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,29 @@
-const CACHE='tooltrack-v1';
-const FILES=[
-  './tooltrack_pwa.html',
-  './manifest.json',
-  'https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@3.3.0/dist/tabler-icons.min.css',
-  'https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@3.3.0/fonts/tabler-icons.woff2'
+const CACHE = 'tooltrack-v1';
+const ASSETS = [
+  '/ToolTrack-พนักงาน.html',
+  'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@400;500;600;700&family=Space+Grotesk:wght@500;600;700&display=swap',
+  'https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/10.12.0/firebase-database-compat.js'
 ];
 
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES).catch(()=>{})));
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
-self.addEventListener('fetch',e=>{
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-      if(res&&res.status===200&&e.request.url.startsWith('http')){
-        const clone=res.clone();
-        caches.open(CACHE).then(c=>c.put(e.request,clone));
-      }
-      return res;
-    }).catch(()=>caches.match('./tooltrack_pwa.html')))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
